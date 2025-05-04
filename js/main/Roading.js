b6993375c281004481c447d68cc07a2ff7d65926
erayuki-lmd <erayuki@lmd.ist.hokudai.ac.jp>
2025-05-04 09:57:40 +0900

    Make_RoadingScrean

--- js/main/Roading.js
+++ js/main/Roading.js
@@ -1,6 +1,3 @@
-
-
-
 const RotationIcon = setInterval(async function(){
     $('#RoadingIcon').css('transition','0s');
     $('#RoadingIcon').css('transform','rotate(0deg)');
@@ -8,6 +5,51 @@ const RotationIcon = setInterval(async function(){
         $('#RoadingIcon').css('transition','2s');
         $('#RoadingIcon').css('transform','rotate(360deg)');
         }
-         ,50)
-},4000);
+         ,5)
+},5000);
 
+const ChangeMessage = setInterval(async function(){
+    setTimeout(() => {
+        $('#RoadingMessage').html('Loading.');
+        }
+         ,2000),
+    setTimeout(() => {
+        $('#RoadingMessage').html('Loading..');
+        }
+         ,3000)
+    setTimeout(() => {
+        $('#RoadingMessage').html('Loading...');
+        }
+        ,4000)
+},5000);
+
+
+function Starting(){
+    setTimeout(() => {
+        $('#RoadingScrean').hide();
+        }
+        ,2000)
+}
+
+
+
+// script
+setTimeout(() => {
+    $('#RoadingIcon').css('transition','2s');
+    $('#RoadingIcon').css('transform','rotate(360deg)');
+    }
+     ,0)
+setTimeout(() => {
+    $('#RoadingMessage').html('Loading.');
+    }
+     ,1000),
+setTimeout(() => {
+    $('#RoadingMessage').html('Loading..');
+    }
+     ,2000)
+setTimeout(() => {
+    $('#RoadingMessage').html('Loading...');
+    }
+    ,3000)
+
+window.onload = Starting;
